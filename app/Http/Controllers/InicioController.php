722cc5d8a6e530bc63ac83b89022c6aabc14134b
edward itriago <edwarditriagosub@gmail.com>
2021-06-25 08:40:52 -0400

    configuracion v7

--- app/Http/Controllers/InicioController.php
+++ app/Http/Controllers/InicioController.php
@@ -6,8 +6,30 @@ use Illuminate\Http\Request;
 
 class InicioController extends Controller
 {
+    protected $token = "1749704343:AAGq-2wfQQJOFTfi4vSPlltwSsuRN5OVrrM";
+    protected $website = "https://api.telegram.org/bot1749704343:AAGq-2wfQQJOFTfi4vSPlltwSsuRN5OVrrM";
+
     public function index()
     {
-        return "Hllaaa";
+        try {
+            $update = file_get_contents('php://input');
+            $update = json_decode($update, TRUE);
+
+            $chatId = $update["message"]["chat"]["id"];
+            $chatType = $update["message"]["chat"]["type"];
+            $message = $update["message"]["text"];
+
+            $response = "Binvenido";
+            $this->sendMessage($chatId, $response);
+            print("FIN");
+        } catch (\Exception $e) {
+            return "Falló";
+        }
+    }
+
+    public function sendMessage($chatId, $response)
+    {
+        $url = $this->website.'/sendMessage?chat_id='.$chatId.'&parse_mode=HTML&text='.urlencode($response);
+        file_get_contents($url);
     }
 }
